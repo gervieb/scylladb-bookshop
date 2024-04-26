89d24daeaeb8692df26bd84217bb2c3455661e1a
gervieb <gerviebarczyk@gmail.com>
2024-04-26 04:32:42 +0200

    refactor button component

--- bookshop-frontend/src/components/Button/index.tsx
+++ bookshop-frontend/src/components/Button/index.tsx
@@ -1,15 +1,35 @@
-import { Button, SxProps } from "@mui/material";
+import { Button, SxProps, ButtonProps } from "@mui/material";
+import styles from "src/components/Button/Button.module.css";
 
-interface IButton {
+interface IButton extends ButtonProps {
   title: string;
   onClick: () => void;
   sx?: SxProps;
 }
 
-const ButtonComponent = ({ title, onClick, sx }: IButton) => {
+const ButtonComponent = ({ title, onClick, sx, ...rest }: IButton) => {
   return (
     <div>
-      <Button onClick={onClick} size="small" sx={sx}>
+      <Button
+        onClick={onClick}
+        size="small"
+        className={styles.button}
+        sx={{
+          backgroundColor: "#627254",
+          border: "1px solid #627254",
+          color: "#ffffff",
+          fontSize: "12px",
+          fontWeight: 600,
+          width: "100%",
+          cursor: "pointer",
+          ":hover": {
+            borderColor: "transparent",
+            backgroundColor: "#76885B",
+          },
+          ...sx,
+        }}
+        {...rest}
+      >
         {title}
       </Button>
     </div>
