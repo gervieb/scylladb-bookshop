e2e64606f05bb9e4a5ce8bb4e44ca73575c1e3ca
gervieb <gerviebarczyk@gmail.com>
2024-04-26 04:39:45 +0200

    refactor header component

--- bookshop-frontend/src/components/Header/index.tsx
+++ bookshop-frontend/src/components/Header/index.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
-import styles from "components/Header/Header.module.css";
 import { Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "src/redux/store";
+import { setIsCartOpen } from "src/redux/slices/cartSlice";
+import styles from "src/components/Header/Header.module.css";
 
 const HeaderComponent = () => {
-  const [showCart, setShowCart] = useState<boolean>(false);
+  const dispatch = useDispatch();
+  const { cartData, isCartOpen } = useSelector(
+    (state: RootState) => state.cart
+  );
+
+  const handleCart = () => {
+    if (cartData?.length > 0) {
+      dispatch(setIsCartOpen(!isCartOpen));
+    }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.flexContainer}>
         <Typography variant="h5">ScyllaDB Bookshop</Typography>
-        <Typography variant="subtitle2" onClick={() => setShowCart(!showCart)}>
-          My Cart
+        <Typography
+          variant="subtitle2"
+          className={styles.cart}
+          onClick={handleCart}
+        >
+          My Cart ({cartData.length})
         </Typography>
       </div>
     </div>
